7a01c6b27b69c181033ed39efdacfa7ee2c059f3
giorgitsibadze <141809950+giorgitsibadze@users.noreply.github.com>
2024-07-17 17:44:12 +0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define pb push_back
+#define popb pop_back
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e15;
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+        cout << '\n';
+    }
+    return 0;
+}
